c1ce027f43fdb5675c46bee50e5c66478e56b288
Paula <Paula@DESKTOP-S8CC043>
2024-03-29 10:25:57 -0700

    Add the CRUD operation for Product

---
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,121 @@
+﻿
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using BulkyWeb.DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IUnitOfWork _UnitOfWork;
+        public ProductController(IUnitOfWork UnitOfWork)
+
+        {
+            _UnitOfWork = UnitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> categories = _UnitOfWork.Product.GetAll().ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                _UnitOfWork.Product.Add(obj);
+                _UnitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product ProductFromDb = _UnitOfWork.Product.Get(u => u.ID == id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            /*if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Displayorder cannot exactly match the name");
+            }*/
+            /*if (obj.Name == "test")
+            {
+                ModelState.AddModelError("", "The Displayorder cannot exactly match the name");
+            }*/
+            if (ModelState.IsValid)
+            {
+                _UnitOfWork.Product.Update(obj);
+                _UnitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product ProductFromDb = _UnitOfWork.Product.Get(u => u.ID == id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            /*if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Displayorder cannot exactly match the name");
+            }*/
+            /*if (obj.Name == "test")
+            {
+                ModelState.AddModelError("", "The Displayorder cannot exactly match the name");
+            }*/
+
+            Product? obj = _UnitOfWork.Product.Get(u => u.ID == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _UnitOfWork.Product.Remove(obj);
+            _UnitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
